9695068ead5d9b78c37f8a86004b9f3467467a7b
AlexKaroh <steam.alexkaroh@gmail.com>
2022-10-13 16:42:13 +0400

    feat: solve 2 tasks

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,9 +14,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function calculateHanoi(disksNumber, turnsSpeed) {
+  let turns = Math.floor(2 ** disksNumber - 1);
+  let turn = Math.floor(2 ** disksNumber - 1);
+  let seconds = Math.floor((turn* 3600)/turnsSpeed);
+  let result = {turns, seconds};
+  return result;
+  
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,11 +11,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function getSeason(date) {
+
+  if (date == undefined) {
+    return 'Unable to determine the time of year!';
+  } 
+  
+  let Month = date.getMonth();
+
+  if (Month >= 2 && Month <= 4) {
+    return 'spring'
+  }
+    else if (Month >= 5 && Month <= 7) {
+      return 'summer'
+    }
+      else if (Month >= 8 && Month <= 10) {
+        return 'autumn'
+      }
+        else if (Month >= 11 || Month <= 1) {
+          return 'winter'
+        }
 }
 
-module.exports = {
-  getSeason
-};
+ module.exports = {
+   getSeason
+ };
+ 
